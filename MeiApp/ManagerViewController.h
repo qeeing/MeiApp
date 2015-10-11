566e6d1c562c99d0ce5e51971edc4be386b4568c
Channe <ichanne@qq.com>
2015-10-11 14:45:03 +0800

    订单管理、注册界面

---
+++ MeiApp/ManagerViewController.h
@@ -0,0 +1,13 @@
+//
+//  ManagerViewController.h
+//  MeiApp
+//
+//  Created by QianLei on 15/10/11.
+//  Copyright (c) 2015年 QianLei. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ManagerViewController : UIViewController
+
+@end
