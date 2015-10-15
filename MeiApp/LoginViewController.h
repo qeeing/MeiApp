24d36c614c94df46d65e5635ad50af3139628b06
Channe <ichanne@qq.com>
2015-10-15 23:44:43 +0800

    导航栏统一色，文件结构调整

--- MeiApp/LoginViewController.h
+++ MeiApp/LoginViewController.h
@@ -8,7 +8,7 @@
 
 #import <UIKit/UIKit.h>
 
-@interface LoginViewController : UIViewController
+@interface LoginViewController : QZBaseViewController
 
 - (IBAction)unwindToLogin:(UIStoryboardSegue *)segue;
 
