c70d2c70b2d35f6cb5bcf4543b2fb13fd0c16fe2
Channe <ichanne@qq.com>
2015-10-17 13:35:52 +0800

    统一修改控制器背景色

---
+++ MeiApp/QZBaseTableView.h
@@ -0,0 +1,13 @@
+//
+//  QZBaseTableView.h
+//  MeiApp
+//
+//  Created by QianLei on 15/10/17.
+//  Copyright (c) 2015年 QianLei. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface QZBaseTableView : UITableView
+
+@end
