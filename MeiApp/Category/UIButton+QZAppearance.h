62b7fbaafb5b0ca061b615fa172575df2262df0d
Channe <ichanne@qq.com>
2015-10-17 12:01:49 +0800

    让 UIAppearance 支持设置按钮圆角

---
+++ MeiApp/Category/UIButton+QZAppearance.h
@@ -0,0 +1,15 @@
+//
+//  UIButton+QZAppearance.h
+//  MeiApp
+//
+//  Created by QianLei on 15/10/17.
+//  Copyright (c) 2015年 QianLei. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UIButton (QZAppearance)
+
+@property (readwrite, nonatomic) CGFloat cornerRadius UI_APPEARANCE_SELECTOR;
+
+@end
--- Readme.md
+++ Readme.md
@@ -1,3 +1,4 @@
 MeiApp 特点：
 
 1，基于多个 StoryBoard 开发：用代码在2个 StoryBoard 间相互调用；
+2，使用 UIApearance 统一修改界面；
