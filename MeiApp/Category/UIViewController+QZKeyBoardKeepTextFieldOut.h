0411525daba6859285338b45e0e76fc0573c293b
qeeing <QianLei@QianLei-MBP.local>
2015-10-24 19:39:32 +0800

    键盘遮挡界面

---
+++ MeiApp/Category/UIViewController+QZKeyBoardKeepTextFieldOut.h
@@ -0,0 +1,13 @@
+//
+//  UIViewController+QZKeyBoardKeepTextFieldOut.h
+//  MeiApp
+//  键盘遮挡UITextField的解决办法
+//  Created by QianLei on 15/10/24.
+//  Copyright (c) 2015年 QianLei. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UIViewController (QZKeyBoardKeepTextFieldOut)
+-(void)resumeView;
+@end
